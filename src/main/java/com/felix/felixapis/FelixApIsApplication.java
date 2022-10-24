002158d12d432c3a8876a1b82fb7512354e823c6
Ratnesh Mishra <ratneshmishrarulz@gmail.com>
2022-10-24 15:31:20 +0530

    First commit

---
+++ src/main/java/com/felix/felixapis/FelixApIsApplication.java
@@ -0,0 +1,13 @@
+package com.felix.felixapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelixApIsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FelixApIsApplication.class, args);
+    }
+
+}
