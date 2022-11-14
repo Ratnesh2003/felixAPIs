99037f1d6ef6c83815433769bff0c3cfdda8f9e4
Janhavi <janhavi25verma@gmail.com>
2022-11-15 00:55:57 +0530

    edit profile controller

--- src/main/java/com/felix/felixapis/controllers/auth/UserProfileController.java
+++ src/main/java/com/felix/felixapis/controllers/auth/UserProfileController.java
@@ -4,7 +4,10 @@ import com.felix.felixapis.payload.response.UserInfoResponse;
 import com.felix.felixapis.security.jwt.JwtUtil;
 import com.felix.felixapis.services.auth.ProfileServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -25,6 +28,12 @@ public class UserProfileController {
         String requestTokenHeader = httpRequest.getHeader("Authorization");
         String email = jwtUtil.getEmailFromToken(requestTokenHeader.substring(7));
         return profileServices.getUserProfile(email);
+    }
+    @PutMapping("/api/change-profile")
+    public ResponseEntity<?> changeProfile(@RequestBody ChangeProfileRequest changeProfileRequest, HttpServletRequest httpRequest){
+        String requestTokenHeader = httpRequest.getHeader("Authorization");
+        String oldEmail = jwtUtil.getEmailFromToken(requestTokenHeader.substring(7));
+        return profileServices.changeUserProfile(changeProfileRequest.getNewFirstName(),changeProfileRequest.getNewLastName(),changeProfileRequest.getNewEmail(),oldEmail);
 
     }
 }
