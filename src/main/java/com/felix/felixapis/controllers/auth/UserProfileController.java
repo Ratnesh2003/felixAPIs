e0800a3200d57765ffbb57764a747d43e0e2a599
Janhavi <janhavi25verma@gmail.com>
2022-11-17 23:22:42 +0530

    Added send verification email

--- src/main/java/com/felix/felixapis/controllers/auth/UserProfileController.java
+++ src/main/java/com/felix/felixapis/controllers/auth/UserProfileController.java
@@ -1,15 +1,15 @@
 package com.felix.felixapis.controllers.auth;
 
+import com.felix.felixapis.payload.request.ChangeProfileRequest;
 import com.felix.felixapis.payload.response.UserInfoResponse;
 import com.felix.felixapis.security.jwt.JwtUtil;
 import com.felix.felixapis.services.auth.ProfileServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
@@ -33,7 +33,18 @@ public class UserProfileController {
     public ResponseEntity<?> changeProfile(@RequestBody ChangeProfileRequest changeProfileRequest, HttpServletRequest httpRequest){
         String requestTokenHeader = httpRequest.getHeader("Authorization");
         String oldEmail = jwtUtil.getEmailFromToken(requestTokenHeader.substring(7));
-        return profileServices.changeUserProfile(changeProfileRequest.getNewFirstName(),changeProfileRequest.getNewLastName(),changeProfileRequest.getNewEmail(),oldEmail);
+        return profileServices.changeUserProfile(changeProfileRequest.getNewFirstName(),changeProfileRequest.getNewLastName(),oldEmail);
 
+    }
+    @GetMapping("/api/change-profile/send-email-verification/newEmail=?1")
+    public ResponseEntity<?> changeEmail(@RequestParam("newEmail") String newEmail,HttpServletRequest httpRequest) throws MessagingException {
+
+        return profileServices.changeUserEmail(newEmail,httpRequest);
+    }
+    @GetMapping("/api/change-profile/save-new-email")
+    public ResponseEntity<?> saveNewEmail(@RequestParam("newEmail")String newEmail,@RequestParam("token")String confirmationToken,HttpServletRequest httpRequest){
+        String requestTokenHeader = httpRequest.getHeader("Authorization");
+        String oldEmail = jwtUtil.getEmailFromToken(requestTokenHeader.substring(7));
+        return profileServices.saveNewEmail(newEmail,oldEmail,confirmationToken);
     }
 }
